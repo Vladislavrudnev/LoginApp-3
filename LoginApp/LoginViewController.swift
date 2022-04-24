bd6dd2da0b807359b6fda36c9e598ee742d06a25
emoge <vlad.rudnev.1992@mail.ru>
2022-04-24 11:58:16 +0600

    Initial Commit

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Alexey Efimov on 19/06/2019.
+//
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    // MARK: - Private properties
+    private let user = "User"
+    private let password = "Password"
+    
+    
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.user = user
+    }
+    
+    // MARK: IBActions
+    @IBAction func logInPressed() {
+        if userNameTextField.text != user || passwordTextField.text != password {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password",
+                textField: passwordTextField
+            )
+        }
+        sh]
+        
+    }
+    
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(title: "Oops!", message: "Your name is \(user) 😉")
+            : showAlert(title: "Oops!", message: "Your password is \(password) 😉")
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+}
+
+// MARK: - Private Methods
+
+
+// MARK: - UITextFieldDelegate
+
+}
